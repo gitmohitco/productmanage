86c138583beecc7fe385bf32aab1e90e35f6b65a
gitmohitco <152581299+gitmohitco@users.noreply.github.com>
2024-09-18 13:30:44 +0530

    Update DBInfo.java

--- OnlineProductStore_App/src/main/java/productcode/DBInfo.java
+++ OnlineProductStore_App/src/main/java/productcode/DBInfo.java
@@ -3,5 +3,5 @@ package productcode;
 public interface DBInfo {
 	public static final String dburl = "jdbc:oracle:thin:@localhost:1521:orcl";
 	public static final String dbuname = "MYDB2PM";
-	public static final String dbpass = "MYDB2PM";
+	public static final String dbpass = "MOHIT";
 }
